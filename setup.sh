ef2c3c5ced9ddf94c1d2cbfb0ee921c12ad8a649
Sean Scannell <sscanne2@nd.edu>
2017-07-26 20:27:37 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ mkdir -p $save_dir'/results/'
 chown -R "$USER" $save_dir
 
 # copy train and test data with proper naming
-data_dir='./tf_seq2seq_chatbot-master/tf_seq2seq_chatbot/data/train'
+data_dir='.'
 cp $data_dir'/movie_lines_selected.txt' $save_dir'/data/chat.in'
 cp $data_dir'/movie_lines_selected_10k.txt' $save_dir'/data/chat_test.in'
